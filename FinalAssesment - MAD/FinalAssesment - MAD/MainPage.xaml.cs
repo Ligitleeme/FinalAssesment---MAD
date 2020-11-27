604224449cfd7ff0739ab0ca788e276ef8d60585
Ligitleeme <30965644+Ligitleeme@users.noreply.github.com>
2020-11-27 18:11:05 +1300

    Add project files.

---
+++ FinalAssesment - MAD/FinalAssesment - MAD/MainPage.xaml.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace FinalAssesment___MAD
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            var image = new Image { Source = "texas_rangers_logo.png" };
+        }
+    }
+}
